cd12b2804c154b1dbf3b89e7dbccab64d87b1438
Manon971 <72388915+Manon971@users.noreply.github.com>
2022-05-24 09:29:30 +0200

    CreateTable

---
+++ Create_capchat.sql
@@ -0,0 +1,40 @@
+create database Capchat;
+
+use Capchat;
+
+CREATE TABLE Image(
+   IdImage INT,
+   NomImage VARCHAR(50),
+   EmplacementImage VARCHAR(100),
+   Singulier BOOLEAN,
+   Indice VARCHAR(100),      
+   IdJeu INT,
+   PRIMARY KEY(IdImage),
+   FOREIGN KEY(IdJeu) REFERENCES Jeu(IdJeu)
+);
+
+CREATE TABLE Theme(
+   IdTheme INT,
+   NomTheme VARCHAR(50),
+   PRIMARY KEY(IdTheme)
+);
+
+CREATE TABLE Jeu(
+   IdJeu INT,
+   NomJeu VARCHAR(50),
+   EmplacementJeu VARCHAR(100),
+   IdTheme INT,
+   IdArtiste INT,
+   PRIMARY KEY(IdJeu),
+   FOREIGN KEY(IdTheme) REFERENCES Theme(IdTheme),
+   FOREIGN KEY(IdArtiste) REFERENCES Artiste(IdArtiste)
+);
+
+CREATE TABLE Artiste(
+   IdArtiste INT,
+   NomArtiste VARCHAR(50),
+   PrenomArtiste VARCHAR(50),
+   Admail VARCHAR(100),
+   Mdp VARCHAR(100),
+   PRIMARY KEY(IdArtiste)
+);
